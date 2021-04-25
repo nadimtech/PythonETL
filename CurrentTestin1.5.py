a0164a8a2e54850bdbbc24d92773acfb6e248013
nadimtech <65955489+nadimtech@users.noreply.github.com>
2021-04-25 14:18:41 +0100

    Update CurrentTestin1.5.py

--- CurrentTestin1.5.py
+++ CurrentTestin1.5.py
@@ -1,53 +1,49 @@
 import pandas as pd
 
-# Create ExcelFile
-excel_file = pd.ExcelFile('stackoverflow.xlsx')
+excel_file = pd.ExcelFile('stack.xlsx')
 type(excel_file)
 excel_file.sheet_names
 
-# Parse into DataFrame
 excel_df = excel_file.parse()
 type(excel_df)
 excel_df.head()
 excel_df
 
-# Or use pandas directly to load worksheet, with read_excel
-posts_excel = pd.read_excel('stackoverflow-one.xlsx')
+posts_excel = pd.read_excel('stack-one.xlsx')
 type(posts_excel)
 dir(posts_excel)
 posts_excel.columns
 posts_excel.head()
-pd.read_excel('stackoverflow-one.xlsx', usecols=[0, 3]).columns
-pd.read_excel('stackoverflow-one.xlsx', usecols='A:C').columns
-pd.read_excel('stackoverflow-one.xlsx', usecols='A,C').columns
+pd.read_excel('stack-one.xlsx', usecols=[0, 3]).columns
+pd.read_excel('stack-one.xlsx', usecols='A:C').columns
+pd.read_excel('stack-one.xlsx', usecols='A,C').columns
 
 # Get a dict of worksheets
 excel_file.sheet_names
-posts_dict = pd.read_excel('stackoverflow.xlsx',sheet_name=None)
+posts_dict = pd.read_excel('stack.xlsx',sheet_name=None)
 type(posts_dict)
 posts_dict.keys()
 posts_dict['Posts'].head()
 
 # Different ways of getting the data you need
 posts_dict['Users'].head()
-pd.read_excel('stackoverflow.xlsx',sheet_name='Users').head()
-pd.read_excel('stackoverflow.xlsx',sheet_name='Users', usecols=range(1,9)).head()
-pd.read_excel('stackoverflow.xlsx',sheet_name=2).head()
-pd.read_excel('stackoverflow.xlsx',sheet_name='Users', usecols=range(1,9)).head()
-pd.read_excel('stackoverflow.xlsx',sheet_name='Users', usecols=range(1,9),skiprows=4).head()
-pd.read_excel('stackoverflow.xlsx',sheet_name='Users', usecols=range(1,9),nrows=2).head()
-pd.read_excel('stackoverflow.xlsx',sheet_name='Users', usecols=range(1,9)).dtypes
-pd.read_excel('stackoverflow.xlsx',sheet_name='Users', usecols=range(1,9), dtype={'PostTypeId': str}).dtypes
-pd.read_excel('stackoverflow.xlsx',sheet_name='Users', converters={'Id': lambda x: x + 1000}).head()
-pd.read_excel('stackoverflow.xlsx',sheet_name='Posts', usecols=[0,7,8]).head()
-pd.read_excel('stackoverflow.xlsx',sheet_name='Posts', usecols=[0,7,8], keep_default_na=False).head()
+pd.read_excel('stack.xlsx',sheet_name='Users').head()
+pd.read_excel('stack.xlsx',sheet_name='Users', usecols=range(1,9)).head()
+pd.read_excel('stack.xlsx',sheet_name=2).head()
+pd.read_excel('stack.xlsx',sheet_name='Users', usecols=range(1,9)).head()
+pd.read_excel('stack.xlsx',sheet_name='Users', usecols=range(1,9),skiprows=4).head()
+pd.read_excel('stack.xlsx',sheet_name='Users', usecols=range(1,9),nrows=2).head()
+pd.read_excel('stack.xlsx',sheet_name='Users', usecols=range(1,9)).dtypes
+pd.read_excel('stack.xlsx',sheet_name='Users', usecols=range(1,9), dtype={'PostTypeId': str}).dtypes
+pd.read_excel('stack.xlsx',sheet_name='Users', converters={'Id': lambda x: x + 1000}).head()
+pd.read_excel('stack.xlsx',sheet_name='Count', usecols=[0,7,8]).head()
+pd.read_excel('stack.xlsx',sheet_name='Count', usecols=[0,7,8], keep_default_na=False).head()
 
 # Import data with Psycopg2
-# psql
+# sql
 # \l
 # \c importing_postgres
 # \d
-# SELECT "Id", "Title" FROM posts LIMIT 5;
 import psycopg2
 stack_connection = psycopg2.connect("dbname=importing_postgres user=xavier host=localhost")
 so_cursor = stack_connection.cursor()
