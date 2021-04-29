83baef18e33159c72b2053b168e6469f6586f6bc
nadimtech <65955489+nadimtech@users.noreply.github.com>
2021-04-29 08:01:47 +0100

    Update Pyton Script for SQl Server.sql

--- Pyton Script for SQl Server.sql
+++ Pyton Script for SQl Server.sql
@@ -1,11 +1,4 @@
-sp_configure 'external scripts enabled', 1;
-RECONFIGURE WITH OVERRIDE
 
-----restart and run
-EXEC sp_configure 'external scripts enabled'
-
-
-----Test 1
 EXECUTE sp_execute_external_script
 @language = N'Python',
 @script = N'
